ac4b21af5fb21a0dac26cadc86fc6d218bfbdeb3
Leo Biaggi <lightbiaggi@gmail.com>
2021-06-13 19:13:31 -0400

    first change

---
+++ database/db.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE dblinks;
+
+USE dblinks;
+
+-- TABLE USER
+-- all pasword wil be encrypted using SHA2
+CREATE TABLE users (
+  id INT(11) NOT NULL,
+  username VARCHAR(16) NOT NULL,
+  email VARCHAR[50] NOT NULL,
+  password VARCHAR(60) NOT NULL,
+  fullname VARCHAR(100) NOT NULL
+);
+
+ALTER TABLE users
+  ADD PRIMARY KEY (id);
+
+ALTER TABLE users
+  MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+DESCRIBE users;
+
+INSERT INTO users (id, username,email, password, fullname) 
+  VALUES (1, 'john', 'john@email.com',  'password1', 'John Carter');
+
+SELECT * FROM users;
+
+-- LINKS TABLE
+CREATE TABLE links (
+  id INT(11) NOT NULL,
+  title VARCHAR(150) NOT NULL,
+  url VARCHAR(255) NOT NULL,
+  ratings INT(1),
+  comments TEXT,
+  user_id INT(11),
+  created_at timestamp NOT NULL DEFAULT current_timestamp,
+  CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+ALTER TABLE links
+  ADD PRIMARY KEY (id);
+
+ALTER TABLE links
+  MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+DESCRIBE links;
